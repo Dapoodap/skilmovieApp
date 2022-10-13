ab412ef6fc98189391141a3e5b717ff0f6dd8656
DaffaRadh <daffasven@gmail.com>
2022-10-13 14:59:04 +0700

    styling dan fix see all

--- main.js
+++ main.js
@@ -1,64 +1,159 @@
+//definite the things indeed
 let firstContainer = document.getElementById("containerPrimary");
 let boxCard = document.getElementById("boxCard")
 let form = document.getElementById("form")
 let inputSearch = document.getElementById("search")
 firstContainer.appendChild(boxCard)
-let key_utama= "https://api.themoviedb.org/3/discover/movie?api_key=5e2a26020fb6095804c3de87a000d457&sort_by=popularity.desc&page=1"
+let key_utama = "https://api.themoviedb.org/3/discover/movie?api_key=5e2a26020fb6095804c3de87a000d457&sort_by=popularity.desc&page=1"
 let baseline = "https://image.tmdb.org/t/p/original" //buat path
 let key_nowP = "https://api.themoviedb.org/3/movie/now_playing?api_key=5e2a26020fb6095804c3de87a000d457&language=en-US&page=1"
-let genre_list = "https://api.themoviedb.org/3/genre/movie/list?api_key=5e2a26020fb6095804c3de87a000d457&language=en-US"
-function getData(key) {
-    fetch(key).then((res)=>{
+let keyRn;
+
+
+
+// create elemen and get data section
+//get part of data
+function getDataSlice(key) {
+    keyRn = key
+    fetch(key).then((res) => {
         return res.json()
-    }).then((res)=>{
+    }).then((res) => {
         let listPilem = res.results
-        listPilem.map((item)=>{
+        listPilem.slice(0, 9).map((item) => {
+            //create for date release
+            const month = new Date(item.release_date)
+            const fixedMonth = month.toLocaleString('en-US', { month: 'long' })
             //create elemen
-                //col
+            //col
             let col = document.createElement("div")
             col.className = "col-auto"
-                //card
+            //card
             let card = document.createElement("div")
             card.className = "card"
             card.style.width = "18rem"
-            card.style.backgroundColor ="#0f0f0f"
+            card.style.backgroundColor = "#0f0f0f"
             card.style.border = "none"
-            card.style.margin ="10px"
-            card.style.cursor ="pointer"
-                //img
+            card.style.margin = "10px"
+            card.style.cursor = "pointer"
+            card.style.overflow = "hidden"
+            card.style.background = "linear-gradient(145deg, #0e0e0e, #101010);"
+            card.style.boxShadow = "8px 8px 16px #0a0a0a, -8px -8px 16px #141414"
+            //img
             let image = document.createElement("img")
             image.className = "card-img-top"
-            image.src = baseline+item.poster_path
+            image.src = baseline + item.poster_path
             image.style.height = "350px"
             image.style.borderRadius = "30px"
-                //card-body
+            image.alt = item.original_title
+            //card-body
             let cardBody = document.createElement("div")
-            cardBody.className ="card-body"
+            cardBody.className = "card-body"
             cardBody.style.color = "wheat"
-            cardBody.style.padding = "15px 8px"
-                //isicardbody
+            cardBody.style.zIndex = "1"
+            cardBody.style.padding = "20px 8px 5px 8px"
+
+            //cardbody
             let containerBody = document.createElement("div")
-            containerBody.className ="container"
+            containerBody.className = "container"
             containerBody.style.padding = "0"
-            containerBody.style.display ="flex"
-            containerBody.style.justifyContent ="space-between"
-                //isi judul dll
+            containerBody.style.display = "flex"
+            containerBody.style.justifyContent = "space-between"
+            //isi judul dll
             let judulContainer = document.createElement("div")
             let judul = document.createElement("p")
             judul.innerText = item.title
             judulContainer.appendChild(judul)
             judul.style.fontSize = "16px"
-            judul.style.fontWeight = "500"
+            judul.style.fontWeight = "600"
+            judul.style.width = "200px"
             let rateContainer = document.createElement("div")
             let rate = document.createElement("p")
             rate.innerText = item.vote_average
             rate.style.fontSize = "16px"
-            rate.style.fontWeight = "300"
+            rate.style.fontWeight = "400"
             rateContainer.appendChild(rate)
             let date = document.createElement("p")
-            date.innerText =item.release_date
+            date.innerText = `${fixedMonth} ${month.getDate()}, ${month.getFullYear()}`
             date.style.fontSize = "16px"
             date.style.fontWeight = "200"
+            date.style.width = "200px"
+
+            //assign ke html
+            boxCard.appendChild(col)
+            col.appendChild(card)
+            card.appendChild(image)
+            card.appendChild(cardBody)
+            cardBody.appendChild(containerBody)
+            containerBody.appendChild(judulContainer)
+            containerBody.appendChild(rateContainer)
+            cardBody.appendChild(date)
+        })
+    })
+}
+    //get data full
+function getDataFull(key) {
+    fetch(key).then((res) => {
+        return res.json()
+    }).then((res) => {
+        let listPilem = res.results
+        listPilem.map((item) => {
+
+            //for release date
+            const month = new Date(item.release_date)
+            const fixedMonth = month.toLocaleString('en-US', { month: 'long' })
+            //create elemen
+            //col
+            let col = document.createElement("div")
+            col.className = "col-auto"
+            //card
+            let card = document.createElement("div")
+            card.className = "card"
+            card.style.width = "18rem"
+            card.style.backgroundColor = "#0f0f0f"
+            card.style.border = "none"
+            card.style.margin = "10px"
+            card.style.cursor = "pointer"
+            card.style.overflow = "hidden"
+            card.style.background = "linear-gradient(145deg, #0e0e0e, #101010);"
+            card.style.boxShadow = "8px 8px 16px #0a0a0a, -8px -8px 16px #141414"
+            //img
+            let image = document.createElement("img")
+            image.className = "card-img-top"
+            image.src = baseline + item.poster_path
+            image.style.height = "350px"
+            image.style.borderRadius = "30px"
+            image.alt = item.original_title
+            //card-body
+            let cardBody = document.createElement("div")
+            cardBody.className = "card-body"
+            cardBody.style.color = "wheat"
+            cardBody.style.padding = "20px 8px 5px 8px"
+
+            //isicardbody
+            let containerBody = document.createElement("div")
+            containerBody.className = "container"
+            containerBody.style.padding = "0"
+            containerBody.style.display = "flex"
+            containerBody.style.justifyContent = "space-between"
+            //isi judul dll
+            let judulContainer = document.createElement("div")
+            let judul = document.createElement("p")
+            judul.innerText = item.title
+            judulContainer.appendChild(judul)
+            judul.style.fontSize = "16px"
+            judul.style.fontWeight = "600"
+            judul.style.width = "200px"
+            let rateContainer = document.createElement("div")
+            let rate = document.createElement("p")
+            rate.innerText = item.vote_average
+            rate.style.fontSize = "16px"
+            rate.style.fontWeight = "400"
+            rateContainer.appendChild(rate)
+            let date = document.createElement("p")
+            date.innerText = `${fixedMonth} ${month.getDate()}, ${month.getFullYear()}`
+            date.style.fontSize = "16px"
+            date.style.fontWeight = "200"
+            date.style.width = "200px"
 
             //assign ke html
             boxCard.appendChild(col)
@@ -77,35 +172,50 @@ function getData(key) {
         })
     })
 }
-getData(key_utama)
+
+//onclick functionfor popular movie (popular)
 function home() {
     while (boxCard.firstChild) {
         boxCard.removeChild(boxCard.firstChild)
     }
-    document.getElementById("indicate").innerText= "Recent popular"
-    getData(key_utama)
+    document.getElementById("indicate").innerText = "Recent popular"
+    getDataSlice(key_utama)
 }
+
+//onclick function for now playing US movie 
 function nowp() {
     while (boxCard.firstChild) {
         boxCard.removeChild(boxCard.firstChild)
     }
-    document.getElementById("indicate").innerText= "Now Playing"
-    getData(key_nowP)
+    document.getElementById("indicate").innerText = "Now Playing"
+    getDataSlice(key_nowP)
 }
 
-form.addEventListener('submit',(eve)=>{
+//function see all
+function seeall() {
+    while (boxCard.firstChild) {
+        boxCard.removeChild(boxCard.firstChild)
+    }
+    getDataFull(keyRn)
+
+}
+//search event
+form.addEventListener('submit', (eve) => {
     eve.preventDefault()
-    if (inputSearch.value === "" ) {
+    if (inputSearch.value === "") {
         alert("kosong")
     } else {
         while (boxCard.firstChild) {
             boxCard.removeChild(boxCard.firstChild)
         }
-        document.getElementById("indicate").innerText= `result for : ${inputSearch.value}`
+        document.getElementById("indicate").innerText = `result for : ${inputSearch.value}`
         let search_key = `https://api.themoviedb.org/3/search/movie?api_key=5e2a26020fb6095804c3de87a000d457&query=${inputSearch.value}&page=1`
-        getData(search_key)
-        inputSearch.value =""
+        getDataSlice(search_key)
+        inputSearch.value = ""
     }
 })
 
+
+//first approach
+getDataSlice(key_utama)
 
